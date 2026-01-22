ce8cf00cf00a4e56eac12831a6a68b5bbc54e95a
ChristinRaju <2006christinraju@gmail.com>
2026-01-22 10:39:24 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,11 +1,14 @@
+import random
+
+nums = [random.randint(1, 100) for _ in range(5)]
+
 def selection_sort(arr):
+    arr = arr.copy()  
     for i in range(len(arr)):
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[i]:
                 arr[i], arr[j] = arr[j], arr[i]
     return arr
 
-
-nums = [64, 25, 12, 22, 11]
 print("Original array:", nums)
 print("Sorted array:", selection_sort(nums))
